c0064755540bfde7064b01432acb0f05e0c8062a
Farkhod95 <misirovfarkhod@gmail.com>
2026-02-26 12:34:16 +0500

    WWW

--- suppliers/serializer/purchase_invoice.py
+++ suppliers/serializer/purchase_invoice.py
@@ -10,6 +10,7 @@ class PurchaseInvoiceListSerializer(serializers.ModelSerializer):
     filial_detail = FilialListSerializer(source='filial', read_only=True)
     supplier_detail = SupplierListSerializer(source='supplier', read_only=True)
     sklad_detail = SkladListSerializer(source='sklad', read_only=True)
+    supplier_debt = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = PurchaseInvoice
@@ -17,6 +18,23 @@ class PurchaseInvoiceListSerializer(serializers.ModelSerializer):
                   'sklad_detail', 'date', 'total_debt_old', 'total_debt', 'total_debt_today', 'product_count',
                   'all_product_summa', 'given_summa_total_dollar', 'given_summa_dollar', 'given_summa_naqt',
                   'given_summa_kilik', 'given_summa_terminal', 'given_summa_transfer', 'is_karzinka')
+
+    def get_supplier_debt(self, obj):
+        if not obj.supplier_id:
+            return Decimal('0.00')
+
+        account = (
+            SupplierAccount.objects
+            .filter(supplier_id=obj.supplier_id)
+            .only('filial_debt')
+            .first()
+        )
+
+        if not account or account.filial_debt is None:
+            return Decimal('0.00')
+
+        return account.filial_debt
+
 
 class PurchaseInvoiceSerializer(serializers.ModelSerializer):
     supplier_debt = serializers.SerializerMethodField(read_only=True)
