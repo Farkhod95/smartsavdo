1bebeb9e11e3bf416a2a551cdb980685e540be4a
Farkhod95 <misirovfarkhod@gmail.com>
2026-03-04 10:56:09 +0500

    WWW

--- sales/serializer/client.py
+++ sales/serializer/client.py
@@ -1,18 +1,20 @@
 from rest_framework import serializers
 from accounts.serializers import RegionListSerializer, DistrictListPublicSerializer, FilialListSerializer
 from sales.models import Client
+from users.serializers import UserViewListShortSerializer
 
 
 class ClientListSerializer(serializers.ModelSerializer):
     region_detail = RegionListSerializer(source='region', read_only=True)
     district_detail = DistrictListPublicSerializer(source='district', read_only=True)
     filial_detail = FilialListSerializer(source='filial', read_only=True)
+    created_by_detail = UserViewListShortSerializer(source='created_by', read_only=True)
 
     class Meta:
         model = Client
         fields = ('id', 'telegram_id', 'full_name', 'is_active', 'date_of_birthday', 'gender', 'phone_number', 'region',
                   'region_detail', 'district', 'district_detail', 'filial', 'filial_detail', 'total_debt', 'keshbek',
-                  'is_profit_loss', 'type', 'is_delete')
+                  'is_profit_loss', 'type', 'is_delete', 'created_by')
 
 
 class ClientSerializer(serializers.ModelSerializer):
